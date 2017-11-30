39ef5c3ad5f40822b5591b7de4e689b7bc309d15
Pavel Guborevich <pavelhuborevich@gmail.com>
2017-11-30 23:53:14 +0300

    Custom Converter was added

--- frontend/src/main/java/studentsService/converters/CustomConvertersRegister.java
+++ frontend/src/main/java/studentsService/converters/CustomConvertersRegister.java
@@ -1,11 +1,11 @@
 package studentsService.converters;
 
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.convert.support.ConversionServiceFactory;
-import org.springframework.core.convert.support.GenericConversionService;
+        import org.springframework.beans.factory.InitializingBean;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.core.convert.support.ConversionServiceFactory;
+        import org.springframework.core.convert.support.GenericConversionService;
 
-import java.util.Set;
+        import java.util.Set;
 
 public class CustomConvertersRegister implements InitializingBean {
 
