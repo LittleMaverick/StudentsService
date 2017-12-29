6b4f0bf293eafa3b85075cb22cbfd4b8a3b08a79
Pavel Guborevich <pavelhuborevich@gmail.com>
2017-12-29 15:51:26 +0300

    Deletion student(s) fuctionality has been modified

--- backend/src/main/java/studentsservice/service/DeletionService.java
+++ backend/src/main/java/studentsservice/service/DeletionService.java
@@ -3,7 +3,11 @@ package studentsservice.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import studentsservice.entities.AppointStudentEntity;
+import studentsservice.entities.PracticeEntity;
 import studentsservice.entities.StudentEntity;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -15,9 +19,23 @@ public class DeletionService {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private AppointStudentService appointStudentService;
+
+    @Autowired
+    private PracticeService practiceService;
+
     public void deleteStudent(int id){
 
         StudentEntity studentEntity = studentService.findById(id);
+
+        List<AppointStudentEntity> appointStudentEntities = appointStudentService.findByStudentId(id);
+
+        for(AppointStudentEntity appointStudentEntity : appointStudentEntities){
+            PracticeEntity practiceEntity = practiceService.findById(appointStudentEntity.getPracticeId());
+            practiceEntity.setAvailableQuantity(practiceEntity.getAvailableQuantity() + 1);
+            practiceService.save(practiceEntity);
+        }
 
         userService.delete(studentEntity.getUserId());
     }
